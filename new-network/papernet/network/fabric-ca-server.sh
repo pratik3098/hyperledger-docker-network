2d42c9267ad7accdb603236ad766dca9782ba76a
root <root@ip-172-31-23-168.us-east-2.compute.internal>
2019-07-18 18:07:19 +0000

    .

--- new-network/papernet/network/fabric-ca-server.sh
+++ new-network/papernet/network/fabric-ca-server.sh
@@ -1,3 +1,3 @@
 fabric-ca-server init -b admin:adminpw -n ca.papernet.hyperfabrics.com 
 
-fabric-ca-server start -n ca.papernet.hyperfabrics.com &
+fabric-ca-server start -n ca.papernet.hyperfabrics.com 2> ./ca-server.log
